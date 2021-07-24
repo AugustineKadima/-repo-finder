953883898e5b91ed69f9c06c644e44d91bf390d3
C0d3president <kadimasam@gmail.com>
2021-07-24 18:07:09 +0300

    Add form validation

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../classes/user';
 
 @Component({
   selector: 'app-user-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent implements OnInit {
 
-
+ user = new User(0,"")
   submitUserName(){
 
   }
